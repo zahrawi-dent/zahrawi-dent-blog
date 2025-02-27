90a68ed264b004155a0f5884b40018b8fbf080a5
MuhammadSwa <muhammadatefsw@protonmail.com>
2025-02-27 02:36:19 +0200

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   site: "https://zhahrawi-dent.github.io/blog",
+  base: "/blog",
   integrations: [preact()],
 });
